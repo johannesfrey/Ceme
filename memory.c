9955c1d1338fb3502823e06fcfd911ed541f7d79
Johannes Frey <me@johannes-frey.de>
2016-06-28 09:40:32 +0200

    Able to allocate symbol objects

--- memory.c
+++ memory.c
@@ -4,36 +4,45 @@
 
 void init_wellknown_objects()
 {
-  nil_object = (struct object *)(malloc(sizeof(struct any_object)));
-  nil_object->u.any.type = T_NIL;
+    nil_object = (struct object *)(malloc(sizeof(struct any_object)));
+    nil_object->u.any.tag = T_NIL;
 
-  void_object = (struct object *)(malloc(sizeof(struct any_object)));
-  void_object->u.any.type = T_VOID;
-  
-  true_object = (struct object *)(malloc(sizeof(struct any_object)));
-  true_object->u.any.type = T_TRUE;
-  
-  false_object = (struct object *)(malloc(sizeof(struct any_object)));
-  false_object->u.any.type = T_FALSE;
-  
-  eof_object = (struct object *)(malloc(sizeof(struct any_object)));
-  eof_object->u.any.type = T_EOF;
+    void_object = (struct object *)(malloc(sizeof(struct any_object)));
+    void_object->u.any.tag = T_VOID;
+
+    true_object = (struct object *)(malloc(sizeof(struct any_object)));
+    true_object->u.any.tag = T_TRUE;
+
+    false_object = (struct object *)(malloc(sizeof(struct any_object)));
+    false_object->u.any.tag = T_FALSE;
+
+    eof_object = (struct object *)(malloc(sizeof(struct any_object)));
+    eof_object->u.any.tag = T_EOF;
 }
 
-object* alloc_number(int64_t value)
+object_p alloc_number(int64_t value)
 {
-  struct number_object *num_obj;
-  num_obj = (struct number_object *)(malloc(sizeof(struct number_object)));
-  num_obj->type = T_NUMBER;
-  num_obj->value = value;
-  return (object *)num_obj;
+    struct number_object *num_obj;
+    num_obj = (struct number_object *)(malloc(sizeof(struct number_object)));
+    num_obj->tag = T_NUMBER;
+    num_obj->value = value;
+    return (object_p)num_obj;
 }
 
-object* alloc_string(char *value)
+object_p alloc_string(char *value)
 {
-  struct string_object *str_obj;
-  str_obj = (struct string_object *)(malloc(sizeof(struct string_object)));
-  str_obj->type = T_STRING;
-  str_obj->value = value;
-  return (object *)str_obj;
+    struct string_object *str_obj;
+    str_obj = (struct string_object *)(malloc(sizeof(struct string_object)));
+    str_obj->tag = T_STRING;
+    str_obj->value = value;
+    return (object_p)str_obj;
+}
+
+object_p alloc_symbol(char *value)
+{
+    struct symbol_object *sym_obj;
+    sym_obj = (struct symbol_object *)(malloc(sizeof(struct symbol_object)));
+    sym_obj->tag = T_SYMBOL;
+    sym_obj->value = value;
+    return (object_p)sym_obj;
 }
--- memory.h
+++ memory.h
@@ -8,36 +8,43 @@
 //
 
 typedef enum {
-  T_NUMBER,
-  T_STRING,
-  T_NIL,
-  T_VOID,
-  T_TRUE,
-  T_FALSE,
-  T_EOF
-} object_type;
+    T_NUMBER,
+    T_STRING,
+    T_SYMBOL,
+    T_NIL,
+    T_VOID,
+    T_TRUE,
+    T_FALSE,
+    T_EOF
+} object_tag;
 
 struct any_object {
-  object_type type;
+    object_tag tag;
 };
 
 struct number_object {
-  object_type type;
-  int64_t value;
+    object_tag tag;
+    int64_t value;
 };
 
 struct string_object {
-  object_type type;
-  char *value;
+    object_tag tag;
+    char *value;
+};
+
+struct symbol_object {
+    object_tag tag;
+    char *value;
 };
 
 typedef struct object {
-  union {
-    struct any_object any;
-    struct number_object number;
-    struct string_object string;
-  } u;
-} object;
+    union {
+        struct any_object any;
+        struct number_object number;
+        struct string_object string;
+        struct symbol_object symbol;
+    } u;
+} object, *object_p;
 
 //
 // Functions
@@ -49,16 +56,17 @@ void init_wellknown_objects();
 // Allocator functions
 //
 
-object* alloc_number(int64_t value);
-object* alloc_string(char *value);
+object_p alloc_number(int64_t value);
+object_p alloc_string(char *value);
+object_p alloc_symbol(char *value);
 
 //
 // Well-known objects
 
-object* nil_object;
-object* void_object;
-object* true_object;
-object* false_object;
-object* eof_object;
+object_p nil_object;
+object_p void_object;
+object_p true_object;
+object_p false_object;
+object_p eof_object;
 
 #endif
